9cf9977d95a49d760269414f024840e7782e7334
Sanjay K <sanjay@kotagiri.me>
2017-07-15 11:18:51 -0700

    :art: refactor(Remove Redux): From file

--- app/navigation/AppNavigator.js
+++ app/navigation/AppNavigator.js
@@ -1,35 +1,18 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
-import { addNavigationHelpers, StackNavigator } from 'react-navigation';
+import { StackNavigator } from 'react-navigation';
 
 import HomeScreen from '../screens/HomeScreen';
-import LocationScreen from '../screens/LocationScreen';
+import { GooglePlacesAutocomplete } from '../screens/GooglePlacesAutoComplete';
 
-
-const routeConfiguration = {
-  Home: { screen: HomeScreen },
-  Location: { screen: LocationScreen },
-};
-// going to disable the header for now
-const stackNavigatorConfiguration = {
-  headerMode: 'none',
-  initialRouteName: 'Home',
-};
-
-export const AppNavigator = StackNavigator(routeConfiguration, stackNavigatorConfiguration);
-
-const AppWithNavigationState = ({ dispatch, nav }) => (
-  <AppNavigator navigation={addNavigationHelpers({ dispatch, state: nav })} />
-);
-
-AppWithNavigationState.propTypes = {
-  dispatch: PropTypes.func.isRequired,
-  nav: PropTypes.object.isRequired,
-};
-
-const mapStateToProps = state => ({
-  nav: state.nav,
+const AppNavigator = StackNavigator({
+  Home: {
+    screen: HomeScreen,
+    navigationOptions: {
+      header: false,
+    },
+  },
+  Location: {
+    screen: GooglePlacesAutocomplete,
+  },
 });
 
-export default connect(mapStateToProps)(AppWithNavigationState);
+export default AppNavigator;
