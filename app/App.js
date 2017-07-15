611d7d0ee5b8042f564ad0b3542cf2a144796282
Sanjay K <sanjay@kotagiri.me>
2017-07-15 11:15:55 -0700

    :zzz: chore(Styles): Fixed flex container style for whole app

--- app/App.js
+++ app/App.js
@@ -53,8 +53,6 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
     backgroundColor: '#FFFFFF',
   },
 });
