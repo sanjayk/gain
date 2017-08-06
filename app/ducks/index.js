5443ffc16dbfaac065e3fe699c07229ce2bb47c5
Sanjay K <sanjay@kotagiri.me>
2017-08-05 19:22:51 -0700

    :sparkles: feature(new ducks) gPlaces

--- app/ducks/index.js
+++ app/ducks/index.js
@@ -1,7 +1,11 @@
 import { combineReducers } from 'redux';
 
 import mycards from './mycards';
+import googleplaces from './googleplaces';
+import location from './location';
 
 export default combineReducers({
   mycards,
+  googleplaces,
+  location,
 });
